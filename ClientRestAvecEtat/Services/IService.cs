671f67daf3354bb6edc24ad4e6aebbc5d96c27a3
JulesSs05 <jules.silvestri@etu.univ-smb.fr>
2023-02-06 10:53:30 +0100

    création des methodes du WS

---
+++ ClientRestAvecEtat/Services/IService.cs
@@ -0,0 +1,19 @@
+﻿using ClientRestAvecEtat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRestAvecEtat.Services
+{
+    public interface IService
+    {
+        Task<List<Serie>> GetSeriesAsync();
+        Task<HttpResponseMessage> PutSerieAsync(Serie serie, int id);
+        Task<HttpResponseMessage> PostSerieAsync(Serie serie);
+        Task<HttpResponseMessage> DeleteSerieAsync(int id);
+        Task<Serie> GetSerieAsync(int id);
+    }
+}
